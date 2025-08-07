59e13e23772b41aa9a0ce659cd1ae9624f416042
Marley_Schneider <marleysch18@gmail.com>
2025-08-07 09:25:26 -0700

    basic board setup

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,33 @@
+'use client';
+
 import Image from "next/image";
 
+function Square ({x, y}){
+  function handleClick(){
+    console.log("button " + x + y + " been clicked");
+  }
+  return(
+    <button className = "square-button" onClick={handleClick}>CLICK ME</button>
+  )
+}
+
 export default function Home() {
+  let board = Array(8).fill(null).map(() =>(Array(8).fill(null)));
+
+
   return (
     <div >
-      <main >
-      </main>
+      {board.map((row, rowIndex) => {
+        return (
+          <div key={rowIndex}>
+            {row.map((square, squareIndex) => {
+              return (
+                <Square y = {rowIndex}x = {squareIndex} key={squareIndex}/>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   );
 }
