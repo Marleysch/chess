fc5210e7bf050ffdb1dd3989e6077a15866d1bb4
Marley_Schneider <marleysch18@gmail.com>
2025-08-08 15:28:59 -0700

    setup env

--- backend/main.cpp
+++ backend/main.cpp
@@ -7,13 +7,20 @@
 #include "bishop.hpp"
 #include "queen.hpp"
 #include "pawn.hpp"
+#include "crow.h"
 using namespace std;
 
 
 int main(){
     cout << "running main" << endl << endl;
 
+    crow::SimpleApp app;
+
     initialize_game();
+
+    // board[0][0]->move(pair<char, int> {'a', 1>});
+
+    // CROW_ROUTE(app, )
 
     print_board();
 
